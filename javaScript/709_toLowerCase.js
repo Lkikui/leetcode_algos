0ace32299ea7bf3151a1ce2cbb4313bd41ce0bf5
LisaRyland <lisa.ryland@gmail.com>
2019-09-03 14:26:21 -0700

    fixed indentations

--- javaScript/709_toLowerCase.js
+++ javaScript/709_toLowerCase.js
@@ -4,16 +4,16 @@
  */
 var toLowerCase = function(str) {
     var newStr = "";
-  
+
     for (var i = 0; i < str.length; i++) {
-    // check if current character is capital letter
-    if (/[A-Z]/.test(str.charAt(i))) {
-        var currentCharCode = str.charCodeAt(i) //get unicode number
-        newStr += String.fromCharCode(currentCharCode + 32) //capital code + 32 => lowercase code
-    } else {
-      newStr += str.charAt(i)
+        // check if current character is a capital letter
+        if (/[A-Z]/.test(str.charAt(i))) {
+            var currentCharCode = str.charCodeAt(i) //get unicode number
+            newStr += String.fromCharCode(currentCharCode + 32) //capital code + 32 => lowercase code
+        } else {
+            newStr += str.charAt(i)
+        }
     }
-  }
-  
-  return newStr;
+
+    return newStr;
 };
