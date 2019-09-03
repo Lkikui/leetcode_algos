00925d4ad1e7a5873bbaa4a7e23fc8b87a98bb96
LisaRyland <lisa.ryland@gmail.com>
2019-09-03 14:23:25 -0700

    solution without built in methods

--- javaScript/709_toLowerCase.js
+++ javaScript/709_toLowerCase.js
@@ -3,5 +3,17 @@
  * @return {string}
  */
 var toLowerCase = function(str) {
-    
+    var newStr = "";
+  
+    for (var i = 0; i < str.length; i++) {
+    // check if current character is capital letter
+    if (/[A-Z]/.test(str.charAt(i))) {
+        var currentCharCode = str.charCodeAt(i) //get unicode number
+        newStr += String.fromCharCode(currentCharCode + 32) //capital code + 32 => lowercase code
+    } else {
+      newStr += str.charAt(i)
+    }
+  }
+  
+  return newStr;
 };
